4f5d8fcc726d45a6f32c9c50a4fb55bb7736bce7
ahyeonkong <kimah0403@gmail.com>
2024-12-31 08:38:25 +0900

    feat: setup project

---
+++ src/main/java/com/market/saessag/SaessagApplication.java
@@ -0,0 +1,13 @@
+package com.market.saessag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaessagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaessagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/market/saessag/SaessagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.saessag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaessagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
