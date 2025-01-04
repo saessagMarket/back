d390c3b617500dde52a1646bd3bc19da9a8aa5a6
ahyeonkong <kimah0403@gmail.com>
2025-01-05 02:42:04 +0900

    feat: User 엔티티에 권한 필드 추가

--- src/main/java/com/market/saessag/user/entity/User.java
+++ src/main/java/com/market/saessag/user/entity/User.java
@@ -1,26 +1,38 @@
-package com.market.saessag.user.domain;
+package com.market.saessag.user.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Data
+@Setter
+@Getter
+@NoArgsConstructor
 @Table(name = "users")
 public class User {
 
-  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(nullable = false, unique = true) // 이메일 중복 가입 방지
   private String email;
 
+  @Column(nullable = false)
   private String password;
 
   private String profileUrl;
 
+  @Column(nullable = false)
   private String nickname;
+
+  private String role; // 권한
+
+  @Builder
+  public User(String email, String password, String profileUrl, String nickname) {
+    this.email = email;
+    this.password = password;
+    this.profileUrl = profileUrl;
+    this.nickname = nickname;
+  }
 
 }
