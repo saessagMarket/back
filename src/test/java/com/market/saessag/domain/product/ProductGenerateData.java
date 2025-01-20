318e04ddae62eb56bc71f297fbaed770a55f1d27
ahyeonkong <kimah0403@gmail.com>
2025-01-20 20:13:32 +0900

    chore: 환경변수 설정 추가

--- src/test/java/com/market/saessag/domain/product/ProductGenerateData.java
+++ src/test/java/com/market/saessag/domain/product/ProductGenerateData.java
@@ -1,4 +1,4 @@
-package com.market.saessag.product;
+package com.market.saessag.domain.product;
 
 import com.market.saessag.domain.product.entity.Product;
 import com.market.saessag.domain.product.repository.ProductRepository;
@@ -31,11 +31,11 @@ public class ProductGenerateData {
 
         // 임의의 User 생성 및 저장
         User user = User.builder()
-                .email("test11@gmail.com")
+                .email("test@gmail.com")
                 .password("0000")
-                .profileUrl("photo11.jpg")
-                .nickname("테스트 닉네임11")
-                .emailVerified(true)     // 이메일 인증 상태를 true로 설정
+                .profileUrl("photo1.jpg")
+                .nickname("테스트 닉네임")
+                .role("ROLE_USER")
                 .build();
         userRepository.save(user);
 
