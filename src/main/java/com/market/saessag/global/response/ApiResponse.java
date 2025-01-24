3ce06c3cf89c6ca59ebd15073e80c6660e5d941d
limjintaek <jintaek019@gmail.com>
2025-01-24 15:36:23 +0900

    feat. ApiResponse success 메소드 추가

--- src/main/java/com/market/saessag/global/response/ApiResponse.java
+++ src/main/java/com/market/saessag/global/response/ApiResponse.java
@@ -2,10 +2,16 @@ package com.market.saessag.global.response;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @Builder
 public class ApiResponse<T> {
+    private String code;
     private String status;
     private T data;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(HttpStatus.OK.name(), HttpStatus.OK.getReasonPhrase(), data);
+    }
 }
