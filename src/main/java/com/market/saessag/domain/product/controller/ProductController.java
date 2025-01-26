1adf2c3c27a946bcb9325a3142f47a78684e05cb
jeonjunbae <wnsqowid0901@gmail.com>
2025-01-26 23:07:46 +0900

    refactor: 컨트롤러 반환 타입 ApiResponse로 변경
    - 기존의 ResponseEntity<ApiResponse> 였던 반환 구조를 ApiResponse 로 통일
    
    Resolves: #52

--- src/main/java/com/market/saessag/domain/product/controller/ProductController.java
+++ src/main/java/com/market/saessag/domain/product/controller/ProductController.java
@@ -20,7 +20,7 @@ public class ProductController {
 
     //통합 조회 (제목, 닉네임, 정렬기준)
     @GetMapping()
-    public ResponseEntity<ApiResponse<Page<ProductResponse>>> searchProducts(
+    public ApiResponse<Page<ProductResponse>> searchProducts(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
             @RequestParam(required = false) String title,
@@ -28,12 +28,10 @@ public class ProductController {
             @RequestParam(required = false) String sort, //정렬 기준 (price, addedDate 등)
             @RequestParam(defaultValue = "desc") String direction) {
         Page<ProductResponse> product = productService.searchProducts(page, size, title, nickname, sort, direction);
-        ApiResponse<Page<ProductResponse>> response = ApiResponse.<Page<ProductResponse>>builder()
+        return ApiResponse.<Page<ProductResponse>>builder()
                 .status("200")
                 .data(product)
                 .build();
-
-        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     //상세 조회
@@ -50,54 +48,44 @@ public class ProductController {
                 .status("200")
                 .data(productDetail)
                 .build();
-
-        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     //상품 생성
     @PostMapping
-    public ResponseEntity<ApiResponse<ProductResponse>> createProduct(@RequestBody ProductRequest productRequest) {
+    public ApiResponse<ProductResponse> createProduct(@RequestBody ProductRequest productRequest) {
         ProductResponse createdProduct = productService.createProduct(productRequest);
-        ApiResponse<ProductResponse> response = ApiResponse.<ProductResponse>builder()
+        return ApiResponse.<ProductResponse>builder()
                 .status("201")
                 .data(createdProduct)
                 .build();
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     //상품 수정
     @PutMapping("/{productId}")
-    public ResponseEntity<ApiResponse<ProductResponse>> updateProduct(@PathVariable Long productId,
+    public ApiResponse<ProductResponse> updateProduct(@PathVariable Long productId,
                                  @RequestBody ProductRequest productRequest) {
         ProductResponse updatedProduct = productService.updateProduct(productId, productRequest);
-        ApiResponse<ProductResponse> response = ApiResponse.<ProductResponse>builder()
+        return ApiResponse.<ProductResponse>builder()
                 .status("200")
                 .data(updatedProduct)
                 .build();
-
-        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     //상품 삭제
     @DeleteMapping("/{productId}")
-    public ResponseEntity<ApiResponse> deleteProduct(@PathVariable Long productId) {
+    public ApiResponse<Void> deleteProduct(@PathVariable Long productId) {
         boolean isDeleted = productService.deleteProduct(productId);
         if (!isDeleted) {
-            ApiResponse<Void> response = ApiResponse.<Void>builder()
+            return ApiResponse.<Void>builder()
                     .status("404")
                     .data(null)
                     .build();
-
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
         }
 
-        ApiResponse<Void> response = ApiResponse.<Void>builder()
+        return ApiResponse.<Void>builder()
                 .status("204")
                 .data(null)
                 .build();
-
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
     }
 
     // 상품 좋아요
