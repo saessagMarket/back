fd9393d8e0ea970416873181b6fe6957602b66ee
jeonjunbae <wnsqowid0901@gmail.com>
2025-02-05 20:28:11 +0900

    feat: 에러 코드 수정
    - ErrorCode에 429 오류 추가
    - CustomException을 이용하여 에러 출력
    
    Resolves: #66

--- src/main/java/com/market/saessag/global/exception/ErrorCode.java
+++ src/main/java/com/market/saessag/global/exception/ErrorCode.java
@@ -20,7 +20,8 @@ public enum ErrorCode {
     FILE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다"),
     PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 상품입니다"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다"),
-    INVALID_ARGUMENT(HttpStatus.BAD_REQUEST, "잘못된 요청 값입니다");
+    INVALID_ARGUMENT(HttpStatus.BAD_REQUEST, "잘못된 요청 값입니다"),
+    RATE_LIMIT(HttpStatus.TOO_MANY_REQUESTS,"너무 많은 요청이 발생했습니다.");
 
     private final HttpStatus status;
     private final String message;
--- src/main/java/com/market/saessag/global/interceptor/RateLimitInterceptor.java
+++ src/main/java/com/market/saessag/global/interceptor/RateLimitInterceptor.java
@@ -1,5 +1,7 @@
 package com.market.saessag.global.interceptor;
 
+import com.market.saessag.global.exception.CustomException;
+import com.market.saessag.global.exception.ErrorCode;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
@@ -27,7 +29,7 @@ public class RateLimitInterceptor implements HandlerInterceptor {
     }
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
         String ip = request.getRemoteAddr(); // 요청 IP 주소
         String path = request.getRequestURI(); // 요청 URI
         String key = ip + ":" + path;
@@ -46,8 +48,7 @@ public class RateLimitInterceptor implements HandlerInterceptor {
         });
 
         if (requestCounts.get(key).count >= MAX_REQUESTS) {
-            response.setStatus(429);
-            return false;
+            throw new CustomException(ErrorCode.RATE_LIMIT);
         }
         return true;
     }
