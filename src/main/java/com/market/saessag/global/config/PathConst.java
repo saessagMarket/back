5aad4df747a52353e49124cd498dd1b525b466b2
ahyeonkong <kimah0403@gmail.com>
2025-02-09 22:44:35 +0900

    fix: 인증 경로 수정

--- src/main/java/com/market/saessag/global/config/PathConst.java
+++ src/main/java/com/market/saessag/global/config/PathConst.java
@@ -6,20 +6,19 @@ public class PathConst {
     public static final String[] EXCLUDED_PATHS = {
             "/api/sign-up",                    // 회원가입
             "/api/sign-in",                    // 로그인
-            "/api/products",                   // 상품 목록 조회
+            "/api/products/list",              // 상품 목록 조회
             "/error"                           // 에러 페이지
     };
 
     // 인증(로그인)이 필요한 경로들
     public static final String[] AUTHENTICATED_PATHS = {
-            "/api/products/",                   // 상품 등록
-            "/api/products/{id}",               // 상품 수정, 삭제, 상세 조회
+            "/api/products",                    // 상품 등록
+            "/api/products/**",                 // 상품 수정, 삭제, 상세 조회
             "/api/products/{id}/like",          // 상품 좋아요
             "/api/products/bump",               // 상품 끌어올리기
             "/api/products/changeStatus",       // 상품 상태 값 변경
-
             "/api/profile/upload-image",        // 프로필 사진 업로드
-            "/api/profile"                     // 프로필 사진 조회
+            "/api/profile"                      // 프로필 사진 조회
     };
 
     private PathConst() {} // 인스턴스화 방지
