b1eed6a260b7fcfc3d3f06f600c7fb9dffead2ee
jeonjunbae <wnsqowid0901@gmail.com>
2025-02-13 15:09:53 +0900

    feat: 웹 소켓 설정 추가
    - WebSocketConfig
     - STOMP WebSocket 엔드포인트 "/ws" 설정
     - 메시지 전송 시 엔드포인트 "/app" prefix 설정
     - 메시지 받을 시 엔드포인트 "/topic" prefix 설정
    
    - PathConst
     - 로그인 없이 접근 가능하도록 웹 소켓 관련 경로 임시 허용
    
    Resolves: #71

--- src/main/java/com/market/saessag/global/config/PathConst.java
+++ src/main/java/com/market/saessag/global/config/PathConst.java
@@ -8,7 +8,10 @@ public class PathConst {
             "/api/sign-in",                    // 로그인
             "/api/products",                   // 상품 목록 조회
             "/api/products/{id}",              // 상품 상세 조회
-            "/error"                           // 에러 페이지
+            "/error",                          // 에러 페이지
+            "/ws/**",                           // 웹 소켓
+            "/topic/**",
+            "/api/chat/**" // 채팅 관련 (임시)
     };
 
     // 인증(로그인)이 필요한 경로들
