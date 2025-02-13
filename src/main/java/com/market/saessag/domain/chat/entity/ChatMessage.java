839ec48e3412c725fff164c112f4d86eed2e2648
jeonjunbae <wnsqowid0901@gmail.com>
2025-02-13 15:22:26 +0900

    feat: 메시지 Entity, Repository 생성
    Resolves: #71

---
+++ src/main/java/com/market/saessag/domain/chat/entity/ChatMessage.java
@@ -0,0 +1,36 @@
+package com.market.saessag.domain.chat.entity;
+
+import com.market.saessag.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "chat_room_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private ChatRoom chatRoom;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    private String content;
+
+    private LocalDateTime timeStamp;
+}
