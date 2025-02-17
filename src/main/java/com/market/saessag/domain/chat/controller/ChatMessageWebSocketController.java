3431c80e378f5698eafe50dbc3d0cf1d874dcdb8
jeonjunbae <wnsqowid0901@gmail.com>
2025-02-17 17:58:59 +0900

    feat: WebSocket 파일 전송 엔드포인트 추가
    - 파일 전송 엔드포인트 `/chat/{roomId}/sendFile` 추가
    
    Resolves: #71

--- src/main/java/com/market/saessag/domain/chat/controller/ChatMessageWebSocketController.java
+++ src/main/java/com/market/saessag/domain/chat/controller/ChatMessageWebSocketController.java
@@ -1,7 +1,10 @@
 package com.market.saessag.domain.chat.controller;
 
+import com.market.saessag.domain.chat.dto.ChatFileRequest;
+import com.market.saessag.domain.chat.dto.ChatFileResponse;
 import com.market.saessag.domain.chat.dto.ChatMessageRequest;
 import com.market.saessag.domain.chat.dto.ChatMessageResponse;
+import com.market.saessag.domain.chat.service.ChatFileService;
 import com.market.saessag.domain.chat.service.ChatMessageService;
 import com.market.saessag.domain.chat.service.ChatSubscriptionService;
 import com.market.saessag.global.response.ApiResponse;
@@ -14,12 +17,16 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class ChatMessageWebSocketController {
     private final ChatMessageService chatMessageService;
     private final ChatSubscriptionService chatSubscriptionService;
+    private final ChatFileService chatFileService;
 
+    // 메시지 전송
     @MessageMapping("/chat/{roomId}/sendMessage")
     @SendTo("/topic/chat/{roomId}")
     public ResponseEntity<ApiResponse<ChatMessageResponse>> sendMessage(@DestinationVariable Long roomId, @Payload ChatMessageRequest message) {
@@ -28,5 +35,14 @@ public class ChatMessageWebSocketController {
         //오프라인 구독자들에게 메시지 전송
         chatSubscriptionService.sendToOffSubscriber(savedMessage, roomId);
         return ResponseEntity.ok(ApiResponse.success(SuccessCode.OK, savedMessage));
+    }
+
+    // 파일 전송
+    @MessageMapping("/chat/{roomId}/sendFile")
+    @SendTo("/topic/chat/{roomId}")
+    public ResponseEntity<ApiResponse<List<ChatFileResponse>>> sendFile(@DestinationVariable Long roomId, @Payload ChatFileRequest fileRequest) {
+        List<ChatFileResponse> savedFile = chatFileService.saveFile(roomId, fileRequest);
+
+        return ResponseEntity.ok(ApiResponse.success(SuccessCode.OK, savedFile));
     }
 }
