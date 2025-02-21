3dbe11242b936d22c0880df8ecde919c4e9e5732
ahyeonkong <kimah0403@gmail.com>
2025-02-21 22:54:52 +0900

    refactor: 회원가입 관련 API의 ApiResponse 공통 응답 형식 통일화

--- src/main/java/com/market/saessag/domain/user/service/SignUpService.java
+++ src/main/java/com/market/saessag/domain/user/service/SignUpService.java
@@ -1,12 +1,12 @@
-// 회원가입 처리
 package com.market.saessag.domain.user.service;
 
 import com.market.saessag.domain.email.service.EmailService;
 import com.market.saessag.domain.user.dto.SignUpRequest;
 import com.market.saessag.domain.user.entity.User;
 import com.market.saessag.domain.user.repository.UserRepository;
-import com.market.saessag.global.exception.CustomException;
 import com.market.saessag.global.exception.ErrorCode;
+import com.market.saessag.global.response.ApiResponse;
+import com.market.saessag.global.response.SuccessCode;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,29 +20,35 @@ public class SignUpService {
     private final EmailService emailService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    // 회원가입
     @Transactional
-    public void signUp(SignUpRequest signUpRequest) {
+    public ApiResponse<String> signUp(SignUpRequest signUpRequest) {
         String email = signUpRequest.getEmail();
 
         // 1. 이메일 인증 여부 먼저 확인
         if (!emailService.isEmailVerified(email)) {
-            throw new CustomException(ErrorCode.EMAIL_NOT_VERIFIED); // 이메일 미인증 시
+            return ApiResponse.error(ErrorCode.EMAIL_NOT_VERIFIED);
         }
 
         // 2. 이메일 중복 확인
         if (userRepository.existsByEmail(email)) {
-            throw new CustomException(ErrorCode.DUPLICATE_EMAIL); // 중복 이메일
+            return ApiResponse.error(ErrorCode.DUPLICATE_EMAIL);
         }
 
         // 3. 회원가입 진행
-        User user = User.builder()
-                .email(email)
-                .password(bCryptPasswordEncoder.encode(signUpRequest.getPassword()))
-                .nickname(signUpRequest.getNickname())
-                .role("ROLE_USER")
-                .build();
+        try {
+            User user = User.builder()
+                    .email(email)
+                    .password(bCryptPasswordEncoder.encode(signUpRequest.getPassword()))
+                    .nickname(signUpRequest.getNickname())
+                    .role("ROLE_USER")
+                    .build();
 
-        userRepository.save(user);
+            userRepository.save(user);
+
+            return ApiResponse.success(SuccessCode.SIGNUP_SUCCESS);
+        } catch (Exception e) {
+            return ApiResponse.error(ErrorCode.SIGNUP_FAILED);
+        }
     }
-
 }
