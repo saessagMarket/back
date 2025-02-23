5081a5813d9816ea17d106cf9657d46be9e8bdde
ahyeonkong <kimah0403@gmail.com>
2025-02-23 16:42:38 +0900

    refactor: ProductRequest에 Lombok 어노테이션 추가
    - @Getter, @Builder, @NoArgsConstructor, @AllArgsConstructor

--- src/main/java/com/market/saessag/domain/product/dto/ProductRequest.java
+++ src/main/java/com/market/saessag/domain/product/dto/ProductRequest.java
@@ -1,10 +1,15 @@
 package com.market.saessag.domain.product.dto;
 
-import lombok.Getter;
-
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductRequest {
     private String title;
     private Long price;
