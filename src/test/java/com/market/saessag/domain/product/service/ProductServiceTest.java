9db59a9962ac778eb4a1eebc3848eac5491d654c
ahyeonkong <kimah0403@gmail.com>
2025-02-23 16:55:59 +0900

    test: 상품 끌어올리기 테스트 코드 개선
    - 세션 인증 관련 모킹 추가

--- src/test/java/com/market/saessag/domain/product/service/ProductServiceTest.java
+++ src/test/java/com/market/saessag/domain/product/service/ProductServiceTest.java
@@ -2,13 +2,19 @@ package com.market.saessag.domain.product.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.market.saessag.domain.product.dto.ProductRequest;
+import com.market.saessag.domain.product.dto.ProductResponse;
 import com.market.saessag.domain.product.entity.Product;
 import com.market.saessag.domain.product.repository.ProductRepository;
+import com.market.saessag.domain.user.dto.SignInResponse;
 import com.market.saessag.domain.user.entity.User;
+import com.market.saessag.domain.user.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,11 +24,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
-
   @InjectMocks
   private ProductService productService;
+
   @Mock
   private ProductRepository productRepository;
+  @Mock
+  private UserRepository userRepository;
 
   @Test
   void bumpProduct() {
@@ -40,18 +48,26 @@ class ProductServiceTest {
             .user(user)
             .build();
 
-    when(productRepository.findById(anyLong()))
-            .thenReturn(Optional.of(product));
-    when(productRepository.save(any(Product.class)))
-            .thenReturn(product);  // save에 대한 mock 동작 추가
+    HttpServletRequest httpRequest = mock(HttpServletRequest.class);
+    HttpSession session = mock(HttpSession.class);
+    SignInResponse userSession = new SignInResponse(user.getId(), user.getProfileUrl(), user.getEmail(), user.getNickname());
+
+    // 모킹 설정
+    when(httpRequest.getSession()).thenReturn(session);
+    when(session.getAttribute("userProfile")).thenReturn(userSession);
+    when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+    when(productRepository.findById(product.getId())).thenReturn(Optional.of(product));
+    when(productRepository.save(any(Product.class))).thenReturn(product);
 
     // when
-    Product newProduct = productService.bumpProduct(product.getId(), user.getId());
+    ProductResponse response = productService.bumpProduct(product.getId(), httpRequest);
 
     // then
-    assertNotNull(newProduct);
-    assertEquals(product.getId(), newProduct.getId());
-    assertNotNull(newProduct.getBumpAt());
-    verify(productRepository).save(newProduct);
+    assertNotNull(response);
+    assertEquals(product.getId(), response.getProductId());
+    assertNotNull(product.getUpdatedAt());
+    verify(productRepository).findById(product.getId());
+    verify(productRepository).save(product);
+    verify(userRepository).findById(user.getId());
   }
 }
