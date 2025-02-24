d94935c6f21f66b2fb4bd6753ed474b63e77ef2e
ahyeonkong <kimah0403@gmail.com>
2025-02-24 14:06:14 +0900

    refactor: 로그인한 사용자만 사진 업로드 가능하도록 인증 로직 추가
    - HttpServletRequest 추가하여 세션 접근
    - HttpSessionRequiredException 처리를 통한 미인증 사용자 접근 제한
    
    Resolves: #75

--- src/main/java/com/market/saessag/domain/photo/controller/PhotoController.java
+++ src/main/java/com/market/saessag/domain/photo/controller/PhotoController.java
@@ -1,9 +1,12 @@
 package com.market.saessag.domain.photo.controller;
 
+import com.market.saessag.domain.auth.AuthService;
 import com.market.saessag.domain.photo.service.S3Service;
 import com.market.saessag.global.exception.ErrorCode;
 import com.market.saessag.global.response.ApiResponse;
 import com.market.saessag.global.response.SuccessCode;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.HttpSessionRequiredException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,21 +18,27 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/photos")
 public class PhotoController {
+    private final AuthService authService;
     private final S3Service s3Service;
 
-    public PhotoController(S3Service s3Service) {
+    public PhotoController(AuthService authService, S3Service s3Service) {
+        this.authService = authService;
         this.s3Service = s3Service;
     }
 
     @PostMapping("/upload")
-    public ApiResponse<List<String>> uploadPhotos(@RequestParam MultipartFile[] files) {
-        List<String> fileUrls = new ArrayList<>();
+    public ApiResponse<List<String>> uploadPhotos(@RequestParam MultipartFile[] files, HttpServletRequest request) {
         try {
+            authService.getAuthenticatedEmail(request); // 로그인 한 사용자만 사진 업로드 가능
+
+            List<String> fileUrls = new ArrayList<>();
             for (MultipartFile file : files) {
                 String fileUrl = s3Service.uploadFile(file);
                 fileUrls.add(fileUrl);
             }
             return ApiResponse.success(SuccessCode.OK, fileUrls);
+        } catch (HttpSessionRequiredException e) {
+            return ApiResponse.error(ErrorCode.UNAUTHORIZED);
         } catch (IOException e) {
             return ApiResponse.error(ErrorCode.FILE_UPLOAD_ERROR);
         }
