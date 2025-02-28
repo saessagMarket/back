94dd50dccd824821503675a8f6c69a14fdb5868b
jeonjunbae <wnsqowid0901@gmail.com>
2025-02-28 15:07:49 +0900

    fix: 충돌 해결
    Resolves: #71

--- src/main/java/com/market/saessag/global/config/PathConst.java
+++ src/main/java/com/market/saessag/global/config/PathConst.java
@@ -4,11 +4,11 @@ package com.market.saessag.global.config;
 public class PathConst {
     // 인증(로그인)이 필요없는 경로들
     public static final String[] EXCLUDED_PATHS = {
-            "/api/sign-up",                    // 회원가입
+            "/api/sign-up/**",                 // 회원가입 관련
             "/api/sign-in",                    // 로그인
-            "/api/products",                   // 상품 목록 조회
-            "/api/products/{id}",              // 상품 상세 조회
-            "/error",                          // 에러 페이지
+            "/api/products/list",              // 상품 목록 조회
+            "/api/password/find",              // 비밀번호 발급
+            "/error",                           // 에러 페이지
             "/ws/**",                           // 웹 소켓
             "/topic/**",
             "/api/chat/**" // 채팅 관련 (임시)
@@ -16,11 +16,11 @@ public class PathConst {
 
     // 인증(로그인)이 필요한 경로들
     public static final String[] AUTHENTICATED_PATHS = {
-            "/api/products/create",            // 상품 등록
-            "/api/products/{id}/update",       // 상품 수정
-            "/api/products/{id}/delete",       // 상품 삭제
-            "/api/profile/upload-image",        // 프로필 사진 업로드
-            "/api/profile"                     // 프로필 사진 조회
+            "/api/products",                    // 상품 등록
+            "/api/products/**",                 // 상품 수정, 삭제 등
+            "/api/photos/**",                   // 사진 관련
+            "/api/profile/**",                  // 프로필 사진 관련
+            "/api/password/change"              // 비밀번호 변경
     };
 
     private PathConst() {} // 인스턴스화 방지
