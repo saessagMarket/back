fc5526f2412c57e596c74afd9abe63dda587015b
jeonjunbae <wnsqowid0901@gmail.com>
2025-03-16 22:46:10 +0900

    feat: WebSocket 세션 인터셉터 설정
    - WebSocket 연결 시 기존 HTTP 세션을 WebSocket 세션에 저장하는 인터셉터 구현
    - WebSocket 요청 시 세션 정보를 활용할 수 있도록 설정 추가
    
    Resolves: #100

--- src/main/java/com/market/saessag/global/config/WebSocketConfig.java
+++ src/main/java/com/market/saessag/global/config/WebSocketConfig.java
@@ -1,10 +1,12 @@
 package com.market.saessag.global.config;
 
+import com.market.saessag.global.interceptor.WebSocketHandShakeInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -13,7 +15,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         //WebSocket 엔드포인트 설정 (ws://localhost:8080/ws)
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*");
+                .setAllowedOrigins("*")
+                .addInterceptors(new WebSocketHandShakeInterceptor());
     }
 
     @Override
